7c10a7361302702c2dee09627603e98c648798c6
Patrick Yavitz <pyavitz@gmail.com>
2023-06-28 06:50:41 -0400

    `Debian Image Builder`
    general maintenance
    
    Signed-off-by: Patrick Yavitz <pyavitz@gmail.com>

--- install.sh
+++ install.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
+source lib/source
 RED="\e[0;31m"
 GRN="\e[0;32m"
 PNK="\e[0;35m"
 TXT="\033[0m"
 YLW="\e[0;33m"
 FIN="\e[0m"
-ARCH=`uname -m`
-BRANCH=`git branch`
+GIT_BRANCH=`git branch`
 
 echo ""
 echo -en "${TXT}Debian Image Builder:${FIN}"
-echo -e " ${PNK}[${FIN}${GRN}${BRANCH}${FIN}${PNK}]${FIN}"
-if [[ -e "/etc/os-release" ]]; then
-	RELEASE=`cat /etc/os-release | grep -w VERSION_CODENAME | sed 's/VERSION_CODENAME=//g'`
-else
-	RELEASE="none"
-fi
+echo -e " ${PNK}[${FIN}${GRN}${GIT_BRANCH}${FIN}${PNK}]${FIN}"
 
 if [[ `command -v curl` ]]; then
 	:;
@@ -37,15 +32,15 @@ else
 fi
 echo -en "${TXT}Checking Host Machine:${FIN} "
 sleep .50
-if [[ "$RELEASE" == "jammy" ]]; then
+if [[ "$HOST_CODENAMES" == "jammy" ]]; then
 	echo -en "${PNK}[${FIN}${GRN}Ubuntu Jammy Jellyfish${FIN}${PNK}]${FIN}"
 	echo ""
 else
-	if [[ "$RELEASE" == "bullseye" ]]; then
+	if [[ "$HOST_CODENAMES" == "bullseye" ]]; then
 		echo -en "${PNK}[${FIN}${GRN}Debian Bullseye${FIN}${PNK}]${FIN}"
 		echo ""
 	else
-		if [[ "$RELEASE" == "bookworm" ]]; then
+		if [[ "$HOST_CODENAMES" == "bookworm" ]]; then
 			echo -en "${PNK}[${FIN}${GRN}Debian Bookworm${FIN}${PNK}]${FIN}"
 			echo ""
 		else
@@ -58,14 +53,14 @@ else
 fi
 echo ""
 echo -e "${TXT}Starting install${FIN} ..."
-if [[ "$ARCH" == "x86_64" ]] || [[ "$ARCH" == "aarch64" ]]; then
+if [[ "$HOST_ARCH" == "x86_64" ]] || [[ "$HOST_ARCH" == "aarch64" ]]; then
 	:;
 else
-	echo -e "ARCH: $ARCH is not supported by this script."
+	echo -e "ARCH: $HOST_ARCH is not supported by this script."
 	exit 0
 fi
 sleep 1s
-if [[ "$ARCH" == "x86_64" ]]; then
+if [[ "$HOST_ARCH" == "x86_64" ]]; then
 	if [[ `command -v make` ]]; then
 		sudo apt update
 		sudo apt upgrade -y
@@ -77,7 +72,7 @@ if [[ "$ARCH" == "x86_64" ]]; then
 		make ccompile
 	fi
 fi
-if [[ "$ARCH" == "aarch64" ]]; then
+if [[ "$HOST_ARCH" == "aarch64" ]]; then
 	if [[ `command -v make` ]]; then
 		sudo apt update
 		sudo apt upgrade -y
