63bb88a2998ae5568383dd3d7eb7f5a149497594
Patrick Yavitz <pyavitz@xxxxx.com>
2024-07-08 10:35:40 -0400

    files: boot: boot.cmd: `overlay support`
    boot/$PLATFORM/overlays/overlays.txt:
    overlays=somefile someotherfile
    
    This is currently in the testing phase so it may not work on all
    platforms. I'm not even sure if the boot script does.
    
    Signed-off-by: Patrick Yavitz <pyavitz@xxxxx.com>

--- files/boot/boot.cmd
+++ files/boot/boot.cmd
@@ -16,8 +16,22 @@ setenv bootargs "${console} rw root=${uuid} ${rootfstype} ${verbose} fsck.repair
 setenv loading ""
 ${loading} ${devtype} ${devnum}:${distro_bootpart} ${kernel_addr_r} ${fk_kvers} \
 && ${loading} ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} ${fdtdir}/${fdtfile} \
-&& ${loading} ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} ${initrd} \
-&& echo "Booting $bootlabel from ${devtype} ${devnum}:${distro_bootpart} ..." \
+&& ${loading} ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} ${initrd}
+
+fdt addr ${fdt_addr_r}
+fdt resize
+setexpr fdtovaddr ${fdt_addr_r} + ${fdtoverlay_addr_r}
+
+if load ${devtype} ${devnum}:${distro_bootpart} ${fdtovaddr} ${fdtdir}/overlays/overlays.txt \
+	&& env import -t ${fdtovaddr} ${filesize} && test -n ${overlays}; then
+	echo "Loaded overlay.txt: ${overlays}"
+	for ov in ${overlays}; do
+		echo "Overlaying ${ov} ..."
+		load ${devtype} ${devnum}:${distro_bootpart} ${fdtovaddr} ${fdtdir}/overlays/${ov}.dtbo && fdt apply ${fdtovaddr}
+	done
+fi
+
+echo "Booting $bootlabel from ${devtype} ${devnum}:${distro_bootpart} ..." \
 && booti ${kernel_addr_r} ${ramdisk_addr_r}:${filesize} ${fdt_addr_r}
 
 echo "Trying bootm ..." \
