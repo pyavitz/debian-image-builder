baf75f8d88e419366bd2745f5a92f2b668e2371c
Patrick Yavitz <pyavitz@xxxxx.com>
2024-07-18 13:34:16 -0400

    packaging: flash_uboot.sh: `maintenance`
    Signed-off-by: Patrick Yavitz <pyavitz@xxxxx.com>

--- packaging/flash_uboot.sh
+++ packaging/flash_uboot.sh
@@ -7,9 +7,7 @@
 VERBOSITY="0"
 if [ $VERBOSITY -eq 1 ]; then set -x; fi
 
-if [[ -f "/etc/opt/board.txt" ]]; then
-	. /etc/opt/board.txt
-fi
+if [[ -f "/etc/opt/board.txt" ]]; then . /etc/opt/board.txt; fi
 
 # unsupported
 if [[ "$BOARD" == "odroidc1" ]]; then
@@ -45,7 +43,7 @@ fi
 MMC=`findmnt -v -n -o SOURCE / | sed 's/..$//'`
 
 # check node
-if [[ "$MMC" == "/dev/mmcblk0" || "$MMC" == "/dev/mmcblk1" || "$MMC" == "/dev/mmcblk2" ]]; then
+if [[ "$MMC" =~ ^(/dev/mmcblk0|/dev/mmcblk1|/dev/mmcblk2)$ ]]; then
 	# locate target device node
 	if [[ -e "${MMC}boot0" ]]; then EMMC="1"; else EMMC="0"; fi
 else
@@ -56,18 +54,14 @@ fi
 target_device () {
 echo -en "${FAMILY}: " | sed -e 's/\(.*\)/\U\1/'
 echo -e "${DEFAULT_MOTD}" | sed -e 's/\(.*\)/\U\1/'
-if [ $EMMC -eq 1 ]; then
-	echo -en "== eMMC: "
-else
-	echo -en "== SDCARD: "
-fi
+if [ $EMMC -eq 1 ]; then echo -en "── eMMC: "; else echo -en "── SDCARD: "; fi
 echo -e "$MMC"
 }
 
 flash_uboot(){
+target_device
 # allwinner
 if [[ "$FAMILY" == "allwinner" ]] && [[ -f "${DIR}/u-boot-sunxi-with-spl.bin" ]]; then
-	target_device
 	sleep .50
 	# flash binary
 	dd if="${DIR}/u-boot-sunxi-with-spl.bin" of="${MMC}" conv=fsync bs=1024 seek=8
@@ -75,14 +69,12 @@ fi
 
 # amlogic / emmc
 if [[ "$FAMILY" == "amlogic" ]] && [ $EMMC -eq 1 ] && [[ -f "${DIR}/u-boot.bin" ]]; then
-	target_device
 	sleep .50
 	# flash binary
 	dd if="${DIR}/u-boot.bin" of="${MMC}" bs=512 seek=1
 fi
 # amlogic / sdcard
 if [[ "$FAMILY" == "amlogic" ]] && [ $EMMC -eq 0 ] && [[ -f "${DIR}/u-boot.bin.sd.bin" ]]; then
-	target_device
 	sleep .50
 	# flash binary
 	dd if="${DIR}/u-boot.bin.sd.bin" of="${MMC}" conv=fsync bs=1 count=442
@@ -91,14 +83,12 @@ fi
 
 # freescale
 if [[ "$FAMILY" == "freescale" ]] && [[ "$ARCH" == "arm" ]] && [[ -f "${DIR}/sploader.bin" ]] && [[ -f "${DIR}/u-boot.bin" ]]; then
-	target_device
 	sleep .50
 	# flash loader and binary
 	dd if="${DIR}/sploader.bin" of="${MMC}" bs=1k seek=1 conv=sync
 	dd if="${DIR}/u-boot.bin" of="${MMC}" bs=1k seek=69 conv=sync
 fi
 if [[ "$FAMILY" == "freescale" ]] && [[ "$ARCH" == "arm64" ]] && [[ -f "${DIR}/u-boot.bin" ]]; then
-	target_device
 	sleep .50
 	# flash binary
 	dd if="${DIR}/u-boot.bin" of="${MMC}" bs=1024 seek=32 conv=notrunc
@@ -106,13 +96,11 @@ fi
 
 # rockchip
 if [[ "$FAMILY" == "rockchip" ]] && [[ -f "${DIR}/u-boot-rockchip.bin" ]]; then
-	target_device
 	sleep .50
 	# flash binary
 	dd if="${DIR}/u-boot-rockchip.bin" of="${MMC}" seek=64
 fi
 if [[ "$FAMILY" == "rockchip" ]] && [[ -f "${DIR}/idbloader.bin" ]] && [[ -f "${DIR}/u-boot.itb" ]]; then
-	target_device
 	sleep .50
 	# flash loader and binary
 	dd if="${DIR}/idbloader.bin" of="${MMC}" seek=64
@@ -123,7 +111,6 @@ if [[ "$FAMILY" == "samsung" ]] && [[ "$BOARD" == "odroidxu4" ]]; then
 	if [ $EMMC -eq 1 ] && [[ -f "${DIR}/bl1.bin" ]] && [[ -f "${DIR}/bl2.bin" ]] && [[ -f "${DIR}/u-boot.bin" ]] && [[ "${DIR}/tzsw.bin" ]]; then
 		DEVICE=`ls /dev/mmcblk*boot0 | sed 's/^.....//'`
 		echo 0 > /sys/block/${DEVICE}/force_ro
-		target_device
 		sleep .50
 		# flash binaries
 		dd if="${DIR}/bl1.bin" of="/dev/${DEVICE}" seek=0 conv=fsync
@@ -136,7 +123,6 @@ fi
 # samsung / odroid xu4 / sdcard
 if [[ "$FAMILY" == "samsung" ]] && [[ "$BOARD" == "odroidxu4" ]]; then
 	if [ $EMMC -eq 0 ] && [[ -f "${DIR}/bl1.bin" ]] && [[ -f "${DIR}/bl2.bin" ]] && [[ -f "${DIR}/u-boot.bin" ]] && [[ "${DIR}/tzsw.bin" ]]; then
-		target_device
 		sleep .50
 		# flash binaries
 		dd if="${DIR}/bl1.bin" of="${MMC}" seek=1 conv=fsync
@@ -149,7 +135,6 @@ fi
 # spacemit
 if [[ "$FAMILY" == "spacemit" ]] && [[ -f "${DIR}/bootinfo_emmc.bin" ]] && [[ -f "${DIR}/u-boot.bin" ]] && \
 	[[ "${DIR}/fw_dynamic.bin" ]] && [[ "${DIR}/FSBL.bin" ]]; then
-	target_device
 	sleep .50
 	if [ $EMMC -eq 1 ]; then
 		DEVICE=`ls /dev/mmcblk*boot0 | sed 's/^.....//'`
@@ -173,9 +158,7 @@ flash_uboot
 sleep .50
 ROOTFS=`findmnt -v -n -o SOURCE /`
 PARTUUID=$(blkid -o export -- $ROOTFS | sed -ne 's/^PARTUUID=//p')
-if [[ -f "/boot/extlinux/extlinux.conf" ]]; then
-	sed -i "s,root=PARTUUID=[^ ]*,root=PARTUUID=${PARTUUID}," /boot/extlinux/extlinux.conf
-fi
+if [[ -f "/boot/extlinux/extlinux.conf" ]]; then sed -i "s,root=PARTUUID=[^ ]*,root=PARTUUID=${PARTUUID}," /boot/extlinux/extlinux.conf; fi
 echo -e ""
 echo -e "You may now reboot."
 
